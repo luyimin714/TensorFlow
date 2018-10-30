e60b948b147d2fa93537c7cb9e7449a250ca0705
luyimin714 <luyimin714@qq.com>
2018-10-30 18:41:14 +0800

    mnist

---
+++ mnist.py
@@ -0,0 +1,110 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 30 14:58:54 2018
+
+@author: luyimin
+"""
+
+import tensorflow as tf
+import numpy as np
+from tensorflow.examples.tutorials.mnist import input_data
+
+def shuffle_batch(X, y, batch_size):
+    rnd_idx = np.random.permutation(len(X))
+    n_batches = len(X) // batch_size
+    for batch_idx in np.array_split(rnd_idx, n_batches):
+        X_batch, y_batch = X[batch_idx], y[batch_idx]
+        yield X_batch, y_batch
+
+(X_train, y_train), (X_test, y_test) = tf.keras.datasets.mnist.load_data()
+y_train = y_train.astype(np.int32)
+y_test = y_test.astype(np.int32)
+X_valid, X_train = X_train[:5000], X_train[5000:]
+y_valid, y_train = y_train[:5000], y_train[5000:]
+
+height = 28
+width = 28
+channels = 1
+n_inputs = height * width
+
+conv1_fmaps = 32
+conv1_ksize = 3
+conv1_stride = 1
+conv1_pad = "SAME"
+
+conv2_fmaps = 64
+conv2_ksize = 3
+conv2_stride = 1
+conv2_pad = "SAME"
+conv2_dropout_rate = 0.25
+
+pool3_fmaps = conv2_fmaps
+
+n_fc1 = 128
+fc1_dropout_rate = 0.5
+
+n_outputs = 10
+
+tf.reset_default_graph()
+
+with tf.name_scope("inputs"):
+    X = tf.placeholder(tf.float32, shape=(None, height, width), name="X")
+    X_reshaped = tf.reshape(X, shape=(-1, height, width, channels))
+    y = tf.placeholder(tf.int32, shape=(None), name="y")
+    """
+    dropout 训练时为True 测试时为False
+    """
+    training = tf.placeholder_with_default(False, shape=[], name="training")
+
+conv1 = tf.layers.conv2d(X_reshaped, filters=conv1_fmaps, kernel_size=conv1_ksize,
+                         strides=conv1_stride, padding=conv1_pad,
+                         activation=tf.nn.relu, name="conv1")
+conv2 = tf.layers.conv2d(conv1, filters=conv2_fmaps, kernel_size=conv2_ksize,
+                         strides=conv2_stride, padding=conv2_pad,
+                         activation=tf.nn.relu, name="conv2")
+
+with tf.name_scope("pool3"):
+    pool3 = tf.nn.max_pool(conv2, ksize=[1,2,2,1], strides=[1,2,2,1], padding="VALID")
+    pool3_shape = pool3.get_shape().as_list()
+    nodes = pool3_shape[1] * pool3_shape[2] * pool3_shape[3]
+    pool3_flat = tf.reshape(pool3, shape=[-1, nodes])
+    #pool3_flat = tf.reshape(pool3, shape=[-1, pool3_fmaps * 7 * 7])
+    pool3_flat_drop = tf.layers.dropout(pool3_flat, conv2_dropout_rate, training=training)
+
+with tf.name_scope("fc1"):
+    fc1 = tf.layers.dense(pool3_flat_drop, n_fc1, activation=tf.nn.relu, name="fc1")
+    fc1_drop = tf.layers.dropout(fc1, fc1_dropout_rate, training=training)
+
+with tf.name_scope("output"):
+    logits = tf.layers.dense(fc1_drop, n_outputs, name="output")
+    Y_proba = tf.nn.softmax(logits, name="Y_proba")
+
+with tf.name_scope("train"):
+    xentropy = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=logits, labels=y)
+    loss = tf.reduce_mean(xentropy)
+    optimizer = tf.train.AdamOptimizer()
+    training_op = optimizer.minimize(loss)
+ 
+with tf.name_scope("eval"):
+    correct = tf.nn.in_top_k(logits, y, 1)
+    accuracy = tf.reduce_mean(tf.cast(correct, tf.float32))
+
+with tf.name_scope("init_and_save"):
+    init = tf.global_variables_initializer()
+    saver = tf.train.Saver()    
+
+mnist = input_data.read_data_sets("../dataset/mnist/")
+
+n_epochs = 10
+batch_size = 100
+
+with tf.Session() as sess:
+    init.run()
+    for epoch in range(n_epochs):
+        for X_batch, y_batch in shuffle_batch(X_train, y_train, batch_size):
+            sess.run(training_op, feed_dict={X: X_batch, y: y_batch, training: True})
+        acc_train = accuracy.eval(feed_dict={X: X_batch, y: y_batch})
+        acc_val = accuracy.eval(feed_dict={X: X_valid, y: y_valid})
+        print(epoch, "Training acc:", acc_train, "Validation acc:", acc_val)
+        
+        save_path = saver.save(sess, "./model/mnist.ckpt")
